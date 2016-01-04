6f10516cd7b46e804d8f0df1d21a73638b201d84
葛方帅 <gefangshuai@163.com>
2016-01-04 09:51:17 +0800

    code

---
+++ speedy-projects/speedy-projects-demo/src/main/java/io/github/gefangshuai/DemoSpringApplication.java
@@ -0,0 +1,14 @@
+package io.github.gefangshuai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by gefan on 2016/1/4.
+ */
+@SpringBootApplication
+public class DemoSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringApplication.class, args);
+    }
+}
