5717cdb8faa82213302d875f2079cc1dd27191c0
gefangshuai <gefangshuai@163.com>
2017-05-26 09:51:47 +0800

    re

--- speedy-ext-webmvc/src/main/java/com/kanasinfo/ext/mvc/common/SupportController.java
+++ speedy-ext-webmvc/src/main/java/com/kanasinfo/ext/mvc/common/SupportController.java
@@ -3,6 +3,7 @@ package com.kanasinfo.ext.mvc.common;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 
@@ -16,6 +17,7 @@ import java.util.Date;
 public abstract class SupportController {
 
     public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
+    private static final String BUSINESS_EXCEPTION_KEY = "bsExpCode";
 
     protected final Logger logger;
 
@@ -30,6 +32,15 @@ public abstract class SupportController {
         SimpleDateFormat dateFormat = new SimpleDateFormat(this.dateFormat);
         dateFormat.setLenient(false);
         binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+    }
+
+    /**
+     * 将错误信息统一打到页面，Key为"code"
+     * @param object
+     * @param model
+     */
+    protected void addBusinessExceptionAttribute(Object object, Model model) {
+        model.addAttribute(BUSINESS_EXCEPTION_KEY, object);
     }
 
 }
