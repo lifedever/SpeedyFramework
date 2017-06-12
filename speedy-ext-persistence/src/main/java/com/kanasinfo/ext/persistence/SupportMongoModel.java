c63b46179ce6517b10293642a00fefc0e373bfbf
gefangshuai <gefangshuai@163.com>
2017-06-12 16:26:26 +0800

    coding

---
+++ speedy-ext-persistence/src/main/java/com/kanasinfo/ext/persistence/SupportMongoModel.java
@@ -0,0 +1,64 @@
+package com.kanasinfo.ext.persistence;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by gefangshuai on 2015/11/13.
+ */
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class SupportMongoModel implements Serializable {
+    @Id
+    private String id;
+
+    @CreatedDate
+    private Date createdDate;
+
+    @LastModifiedDate
+    private Date lastModifiedDate;
+
+    public SupportMongoModel() {
+        this.id = UUID.randomUUID().toString().replace("-", "");
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    @Transient
+    public boolean isNew() {
+        return id == null;
+    }
+
+    @Transient
+    public boolean isNotNew() {
+        return !isNew();
+    }
+}
---
+++ speedy-ext-persistence/src/main/java/com/kanasinfo/ext/persistence/anno/Mongo.java
@@ -0,0 +1,8 @@
+package com.kanasinfo.ext.persistence.anno;
+
+/**
+ * 特殊地方支持Mongodb标记
+ * Created by gefangshuai on 2017/6/12.
+ */
+public enum Mongo {
+}
