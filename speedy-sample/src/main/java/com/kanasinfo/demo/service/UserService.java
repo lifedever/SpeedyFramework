a9b3294cd4eb84673479458b108a88c4dc201875
gefangshuai <gefangshuai@163.com>
2018-06-16 10:43:40 +0800

    coding

--- speedy-sample/src/main/java/com/kanasinfo/demo/service/UserService.java
+++ speedy-sample/src/main/java/com/kanasinfo/demo/service/UserService.java
@@ -2,7 +2,9 @@ package com.kanasinfo.demo.service;
 
 import com.kanasinfo.demo.dao.UserRepository;
 import com.kanasinfo.demo.model.User;
+import com.kanasinfo.ext.persistence.SupportRepository;
 import com.kanasinfo.ext.persistence.SupportService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -13,15 +15,20 @@ import javax.annotation.Resource;
 @Service
 public class UserService extends SupportService<User, String> {
 
+    @Autowired
     private UserRepository userRepository;
 
     @Resource
     public void setUserDao(UserRepository userRepository) {
         this.userRepository = userRepository;
-        super.supportRepository = userRepository;
     }
 
     public User findByUsername(String username) {
         return userRepository.findByUsername(username);
+    }
+
+    @Override
+    protected SupportRepository<User, String> getRepository() {
+        return this.userRepository;
     }
 }
